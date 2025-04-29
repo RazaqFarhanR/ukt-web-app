60f52e45894dd4e4af6781eb97c0a83044218f9e
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-04-29 21:58:50 +0700

    upd: filter siswa penguji sesuai ranting

--- client/pages/penguji/[name].jsx
+++ client/pages/penguji/[name].jsx
@@ -187,7 +187,8 @@ const detail_event = () => {
                                 console.log('penguji ranting');
                                 return (
                                     <>
-                                        {dataSiswa.filter(dataEvent.tipe_ukt === 'UKCW' ? (a => a.id_event === dataEvent.id_event) : (a => a.id_ranting === dataPenguji.id_ranting && a.id_event === dataEvent.id_event)).map((item, index) => (
+                                        {/* {dataSiswa.filter(dataEvent.tipe_ukt === 'UKCW' ? (a => a.id_event === dataEvent.id_event) : (a => a.id_ranting === dataPenguji.id_ranting && a.id_event === dataEvent.id_event)).map((item, index) => ( */}
+                                        {dataSiswa.filter(a => a.id_ranting === dataPenguji.id_ranting && a.id_event === dataEvent.id_event).map((item, index) => (
                                             <button className='w-full text-left' onClick={() => goToNilai(item)} key={index + 1}>
                                                 <div className="bg-navy rounded-md p-3 text-white mb-3 shadow shadow-slate-700 hover:shadow-purple hover:scale-[1.01] transition ease-in-out duration-300">
                                                     <h1 className='text-green tracking-wide text-lg'>{item.nomor_urut}</h1>
@@ -206,29 +207,30 @@ const detail_event = () => {
                         })()}
                     </div>
                 </div>
-                       <div className="fixed bottom-0 z-50 block w-full">
-                        {/* search */}
-                        <div className="bg-navy py-1.5 px-4 rounded-md gap-x-2 flex items-center ">
-                            {/* button search */}
-                            <button onClick={() => searchSiswa()}>
-                            <svg width="25" height="25" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.625 16.625C13.491 16.625 16.625 13.491 16.625 9.625C16.625 5.75901 13.491 2.625 9.625 2.625C5.75901 2.625 2.625 5.75901 2.625 9.625C2.625 13.491 5.75901 16.625 9.625 16.625Z" stroke="#6464F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d="M18.3746 18.3751L14.5684 14.5688" stroke="#6464F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            </button>
 
-                            {/* input search */}
-                            <input className='w-full p-1.5 focus:outline-none' placeholder='Search' type="text" onChange={(e) => setSearchName(e.target.value)} />
+                <div className="fixed bottom-0 z-50 block w-full">
+                {/* search */}
+                <div className="bg-navy py-1.5 px-4 rounded-md gap-x-2 flex items-center ">
+                    {/* button search */}
+                    <button onClick={() => searchSiswa()}>
+                    <svg width="25" height="25" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9.625 16.625C13.491 16.625 16.625 13.491 16.625 9.625C16.625 5.75901 13.491 2.625 9.625 2.625C5.75901 2.625 2.625 5.75901 2.625 9.625C2.625 13.491 5.75901 16.625 9.625 16.625Z" stroke="#6464F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M18.3746 18.3751L14.5684 14.5688" stroke="#6464F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                    </button>
 
-                            {/* filter button */}
-                            <button onClick={() => setModalFilter(true)}>
-                                <svg width="25" height="25" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M25.6667 3.5H2.33337L11.6667 14.5367V22.1667L16.3334 24.5V14.5367L25.6667 3.5Z" stroke="#6464F6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </button>
-                        </div>
-                        <MainNavigation active={active} setActive={setActive}/>
-                        </div>
+                    {/* input search */}
+                    <input className='w-full p-1.5 focus:outline-none' placeholder='Search' type="text" onChange={(e) => setSearchName(e.target.value)} />
+
+                    {/* filter button */}
+                    <button onClick={() => setModalFilter(true)}>
+                        <svg width="25" height="25" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25.6667 3.5H2.33337L11.6667 14.5367V22.1667L16.3334 24.5V14.5367L25.6667 3.5Z" stroke="#6464F6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+                <MainNavigation active={active} setActive={setActive}/>
+                </div>
             </div>
             
             <globalState.Provider value={{ modalFilter, setModalFilter}}>
